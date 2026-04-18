53c03d5049f0b242afeafa6026aa6564fe0c353b
kawtharaitelhousseine-beep <kawtharaitelhousseine@gmail.com>
2026-04-18 14:20:38 +0200

    Update interfac.py

--- interfac.py
+++ interfac.py
@@ -1,6 +1,6 @@
 # """
 #  OUTIL PAT — Système d'Aide à la Décision 
-  Base CFD : Warman 10/8 M | D2=549mm | H_BEP=37.27m | Q_BEP=905.7 m³/h
+  Base CFD | Warman 10/8 M | D2=549mm | H_BEP=37.27m | Q_BEP=905.7 m³/h
   PFE 2025-2026 | EMI | Weir Minerals North Africa
   streamlit run interfac.py
 """
